1b2202f66a77ad190c4870a1ec4add8a9a8cc18e
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2015-11-18 12:59:42 -0600

    removed unused files

--- LFP.m
+++
@@ -1,9 +0,0 @@
-%{
-example.LFP (imported) # Local Field Potential signal from one electrode 
-->example.Ephys
-channel :int  # channel number
------
-voltage :longblob  # (uV) raw local field potential signal
-%}
-classdef LFP < dj.Relvar
-end
--- LFP.py
+++
@@ -1,8 +0,0 @@
-@schema
-class LFP(dj.Imported):
-definition = """ # Local Field Potential signal from one electrode 
-->example.Ephys
-channel :int  # channel number
------
-voltage :longblob  # (uV) raw local field potential signal
-"""
